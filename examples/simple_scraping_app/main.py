d72c417d344fb22891f00d134278e786363208c4
karosis88 <kar.petrosyanpy@gmail.com>
2023-04-02 04:55:32 -0400

    Add very simple scraping script as an example

---
+++ examples/simple_scraping_app/main.py
@@ -0,0 +1,37 @@
+from scrapyio.engines import Engine
+from scrapyio.spider import BaseSpider
+from scrapyio import Request
+from scrapyio.items import ItemManager
+from scrapyio.item_loaders import JSONLoader
+from scrapyio.items import Item
+from bs4 import BeautifulSoup
+import asyncio
+
+
+class Country(Item):
+    name: str
+    population: int
+    capital: str
+    area: float
+
+
+class Spider(BaseSpider):
+    start_requests = [Request(url="https://www.scrapethissite.com/pages/simple/", method="GET")]
+
+    async def parse(self, response):
+        soup = BeautifulSoup(response.text, "html.parser")
+        countries = soup.select(".country")
+
+        for country in countries:
+            country_name = country.select_one(".country-name").text.strip()
+            country_population = int(country.select_one(".country-population").text.strip())
+            country_capital = country.select_one(".country-capital").text.strip()
+            country_area = float(country.select_one(".country-area").text.strip())
+
+            yield Country(name=country_name, population=country_population, capital=country_capital, area=country_area)
+
+
+if __name__ == "__main__":
+    item_manager = ItemManager(loader=JSONLoader(filename="data.json"))
+    engine = Engine(items_manager=item_manager, spider_class=Spider)
+    asyncio.run(engine.run())
