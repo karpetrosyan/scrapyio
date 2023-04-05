1a8625df03fbace047d021e7317338f1e9928ccc
karosis88 <kar.petrosyanpy@gmail.com>
2023-04-05 11:20:08 +0300

    Add test for the `settings.py`

---
+++ tests/test_settings.py
@@ -0,0 +1,21 @@
+"""
+This module contains scrapyio 'settings' tests
+and ensures that configuration file loading
+and scrapyio configurations work as expected.
+"""
+
+import pytest
+
+
+def test_config_loading_without_test_env(monkeypatch, clear_sys_modules):
+    monkeypatch.delenv("TESTING")
+    with pytest.raises(ModuleNotFoundError):
+        mod = __import__("scrapyio.settings")
+        mod.settings.CONFIGS
+
+
+def test_config_loading_with_test_env(monkeypatch, clear_sys_modules):
+    monkeypatch.setenv("TESTING", "TRUE")
+    mod = __import__("scrapyio.settings")
+    CONFIGS = mod.settings.CONFIGS
+    assert CONFIGS.__name__ == "scrapyio.templates.configuration_template"
