9ca5e166b70dcbb8e7f8cd9466f2bb2d672baecf
karosis88 <kar.petrosyanpy@gmail.com>
2023-04-05 11:18:23 +0300

    Add test for the utils

---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+from scrapyio.utils import load_module
+from scrapyio.utils import random_filename
+
+
+def test_object_loading():
+    assert load_module("scrapyio.middlewares.BaseMiddleWare").__name__ == "BaseMiddleWare"
+    assert load_module("scrapyio.downloader.BaseDownloader").__name__ == "BaseDownloader"
+    assert load_module("scrapyio.utils.load_module").__name__ == "load_module"
+
+
+def test_random_filename():
+    filename = random_filename()
+    assert filename.startswith("dump_")
+    assert len(filename.split("_")[1]) == 4
+
+    filename = random_filename(random_suffix_length=6)
+    assert filename.startswith("dump_")
+    assert len(filename.split("_")[1]) == 6
