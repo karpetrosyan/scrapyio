390a991680b1fe267ccbef1bbccb1b8828b8eae5
karosis88 <kar.petrosyanpy@gmail.com>
2023-04-05 11:16:49 +0300

    Isolate logger creation

---
+++ scrapyio/logger.py
@@ -0,0 +1,8 @@
+from logging import config
+from logging import getLogger
+
+from scrapyio.settings import CONFIGS
+
+config.dictConfig(CONFIGS.DEFAULT_LOGGING_CONFIG)
+first_logger_name = CONFIGS.DEFAULT_LOGGING_CONFIG["loggers"].keys()[0]
+logger = getLogger(first_logger_name)
