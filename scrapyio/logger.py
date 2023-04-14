32becd764e873713930c1b1dc66668728a8360f0
karosis88 <kar.petrosyanpy@gmail.com>
2023-04-14 11:30:47 +0300

    Remove logging file

--- scrapyio/logger.py
+++
@@ -1,7 +0,0 @@
-from logging import config, getLogger
-
-from scrapyio.settings import CONFIGS
-
-config.dictConfig(CONFIGS.DEFAULT_LOGGING_CONFIG)
-first_logger_name = CONFIGS.DEFAULT_LOGGING_CONFIG["loggers"].keys()[0]
-logger = getLogger(first_logger_name)
